e8931698256f27aa4c5facbc59d414daa996cc86
pws019 <whitesmith@whites-MacBook-Pro.local>
2026-06-28 01:44:16 +0800

    initial commit

---
+++ apps/web/src/utils/trpc.ts
@@ -0,0 +1,34 @@
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+import type { AppRouter } from "@token-query/api/routers/index";
+import { env } from "@token-query/env/web";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import { toast } from "sonner";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      toast.error(error.message, {
+        action: {
+          label: "retry",
+          onClick: () => {
+            query.invalidate();
+          },
+        },
+      });
+    },
+  }),
+});
+
+export const trpcClient = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: `${env.VITE_SERVER_URL}/trpc`,
+    }),
+  ],
+});
+
+export const trpc = createTRPCOptionsProxy<AppRouter>({
+  client: trpcClient,
+  queryClient,
+});
